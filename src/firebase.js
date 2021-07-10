fd422b863d1d0aec3be69eff111269ae023d0b9c
Manuel Duran <durbonca@gmail.com>
2021-07-10 11:58:05 -0400

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-import "firebase/firestore";
 import env from "react-dotenv";
+import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: env.APIKEY,
-  authDomain: env.AUTHDOMAIN,
-  projectId: env.PROJECTID,
-  storageBucket: env.STORAGEBUCKET,
-  messagingSenderId: env.MESSAGINGSENDERID,
-  appId: env.APPID
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID
 };
 
 firebase.initializeApp(firebaseConfig);
